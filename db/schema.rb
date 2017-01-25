4d592751d8c6d75bc99880be61d088ec66f16270
Benjamin Yi <bentaebox@gmail.com>
2017-01-25 17:46:19 -0500

    Create muscle model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121052042) do
+ActiveRecord::Schema.define(version: 20170125223413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "scrapers", force: :cascade do |t|
+  create_table "muscles", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
